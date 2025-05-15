a29852c4f0a7589d37035a22ce7c4894bab37448
utkutasglk <152283834+utkutasglk@users.noreply.github.com>
2025-05-15 10:31:09 +0300

    formatting string

---
+++ string_formatting.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	num := 425
+	fmt.Printf("%05d\n", num)
+
+	message := "Hello lkjsdhfgl"
+	fmt.Printf("|%10s|\n", message)
+	fmt.Printf("|%-10s|\n", message)
+
+	message1 := "Hello \nWorld"
+	message2 := `Hello \nWorld`
+
+	fmt.Println(message1)
+	fmt.Println(message2)
+	
+}
